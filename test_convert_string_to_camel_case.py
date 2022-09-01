6900bb12a0f7f8b780f371e9bff492bf1f20043d
5ato <pomidkil@gmai.com>
2022-09-01 20:15:22 +0500

    test_convert_string_to_camel_case

---
+++ test_convert_string_to_camel_case.py
@@ -0,0 +1,17 @@
+"""
+https://www.codewars.com/kata/517abf86da9663f1d2000003/train/python
+"""
+
+import re
+
+
+def to_camel_case(text: str):
+    return text[0] + ''.join(item.title() for item in re.split('[-|_]', text)[1::])
+
+
+def main():
+    print(to_camel_case("the_stealth-warrior"))
+
+
+if __name__ == '__main__':
+    main()
