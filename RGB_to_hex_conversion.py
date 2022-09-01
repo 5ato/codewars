8269b33bb158e357f22cae5ed77f23ba4bfc0bca
5ato <pomidkil@gmai.com>
2022-09-01 20:08:38 +0500

    edit file: RGB to hex conversion

--- RGB_to_hex_conversion.py
+++ RGB_to_hex_conversion.py
@@ -1,5 +1,23 @@
+"""
+https://www.codewars.com/kata/513e08acc600c94f01000001
+"""
+
+
+def rgb1(r, g, b):
+    convert = [i if i > 0 else 0 for i in (r, g, b)]
+    convert = ['%X' % i if i < 255 else 'FF' for i in convert]
+    convert = [i if len(i) == 2 else '0' + i for i in convert]
+    return ''.join(convert)
+
+
+def rgb2(r, g, b):
+    check = lambda x: min(255, max(x, 0))
+    return '%02X%02X%02X' % (check(r), check(g), check(b))
+
+
 def main():
-    pass
-    
+    print(rgb2(-1, 55, 267))
+
+
 if __name__ == '__main__':
     main()
