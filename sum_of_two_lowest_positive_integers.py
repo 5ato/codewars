d03360a25f4a554874c9b2a2339930e2ce9da1ea
5ato <pomidkil@gmai.com>
2022-09-01 20:10:00 +0500

    sum_of_two_lowest_positive_integers

--- sum_of_two_lowest_positive_integers.py
+++ sum_of_two_lowest_positive_integers.py
@@ -1,5 +1,29 @@
+"""
+https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/python
+"""
+
+
+def sum_two_smallest_numbers(numbers: list):
+    return numbers.pop(numbers.index(min(numbers))) + min(numbers)
+
+
+def find_small(numbers):
+    small = numbers[0]
+    small_index = 0
+    for i in range(1, len(numbers)):
+        if small > numbers[i]:
+            small = numbers[i]
+            small_index = i
+    return small_index
+
+
+def sum_two_smallest_numbers2(numbers):
+    return numbers.pop(find_small(numbers)) + numbers.pop(find_small(numbers))
+
+
 def main():
     pass
-    
+
+
 if __name__ == '__main__':
     main()
