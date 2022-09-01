4553238f45013f55fa5f261058e5c2362606dde6
5ato <pomidkil@gmai.com>
2022-09-01 20:15:53 +0500

    which_are_in

--- which_are_in.py
+++ which_are_in.py
@@ -1,5 +1,26 @@
+"""
+https://www.codewars.com/kata/550554fd08b86f84fe000a58
+"""
+
+
+def in_array1(array1, array2):
+    seen = set()
+    for j in array2:
+        for i in array1:
+            if i in j:
+                seen.add(i)
+    return sorted(list(seen))
+
+
+def in_array2(array1, array2):
+    return sorted({sub for sub in array1 if any(sub in s for s in array2)})
+
+
 def main():
-    pass
-    
+    a1 = ["arp", "mice", "bull"]
+    a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+    print(in_array2(a1, a2))
+
+
 if __name__ == '__main__':
     main()
