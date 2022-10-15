8299e33e6a8a1aef1cfa5ef73634489c88cf5064
5ato <pomidkil@gmai.com>
2022-10-16 00:48:48 +0500

    range extraction

---
+++ range_extraction.py
@@ -0,0 +1,26 @@
+from itertools import groupby
+
+"""
+https://www.codewars.com/kata/51ba717bb08c1cd60f00002f
+"""
+
+
+def group(args):
+    for _, g in groupby(enumerate(args), key=lambda x: x[0]-x[-1]):
+        r = [i for _, i in g]
+        if len(r) > 2:
+            yield f'{r[0]}-{r[-1]}'
+        else:
+            yield from map(str, r)
+
+
+def solution(args):
+    return ','.join(group(args))
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
