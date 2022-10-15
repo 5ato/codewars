b1ad7d0d2c30f32b74a9b31748ea1e43495c2f22
5ato <pomidkil@gmai.com>
2022-10-16 00:50:03 +0500

    added docstring

--- range_extraction.py
+++ range_extraction.py
@@ -5,7 +5,7 @@ https://www.codewars.com/kata/51ba717bb08c1cd60f00002f
 """
 
 
-def group(args):
+def group(args: list):
     for _, g in groupby(enumerate(args), key=lambda x: x[0]-x[-1]):
         r = [i for _, i in g]
         if len(r) > 2:
@@ -14,7 +14,7 @@ def group(args):
             yield from map(str, r)
 
 
-def solution(args):
+def solution(args: list) -> str:
     return ','.join(group(args))
 
 
--- roman_numerals_decoder.py
+++ roman_numerals_decoder.py
@@ -1,3 +1,8 @@
+"""
+https://www.codewars.com/kata/51b6249c4612257ac0000005
+"""
+
+
 def solution(roman):
     roman1 = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman2 = {'IV': -2, 'IX': -2, 'XL': -20, 'XC': -20, 'CD': -200, 'CM': -200}
