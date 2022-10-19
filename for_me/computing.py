d7ea6b554ac613c2bb11d3901ccc22f1016d6553
5ato <pomidkil@gmail.com>
2022-10-19 10:06:28 +0500

    fibo and fibo_recursion added to file

--- for_me/computing.py
+++ for_me/computing.py
@@ -1,3 +1,20 @@
+def fibo(diapason: int) -> list:
+    arr = []
+    a, b = 0, 1
+    for i in range(diapason):
+        a, b = b, a+b
+        arr.append(a)
+    return arr
+
+
+# recursion
+def fib_req(diapason: int) -> int:
+    if diapason <= 1:
+        return 1
+    else:
+        return fib_req(diapason-1) + fib_req(diapason-2)
+
+
 # a % b
 def mod(number1: int, number2: int) -> int:
     return number1 - (number1 // number2) * number2
@@ -18,7 +35,7 @@ def gcd(number1: int, number2: int) -> int:
 
 
 def main():
-    print(gcd(400, 320))
+    print(fib_req(13))
 
 
 if __name__ == '__main__':
