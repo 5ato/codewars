ab0d22ac82f7ddb33aeb92f4fe1cbdd1c0bde6a3
5ato <pomidkil@gmail.com>
2022-10-19 11:01:30 +0500

    find mathing letters function added

--- for_me/random.py
+++ for_me/random.py
@@ -1,3 +1,13 @@
+def find_matching_letters(s1: str, s2: str) -> str:
+    hist = {}
+    for ch in "abcdefghijklmnopqrstuvwxyz":
+        val1, val2 = s1.count(ch), s2.count(ch)
+        if max(val1, val2) > 1:
+            which = '1' if val1 > val2 else '2' if val2 > val1 else '='
+            hist = -max(val1, val2), which + ':' + ch * max(val1, val2)
+    return '/'.join(hist[ch][1] for ch in sorted(hist, key=lambda x: hist[x]))
+
+
 def lifo(string: str) -> bool:
     stack = []
     verify = True
