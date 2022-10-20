7555ed80cfa6d6ff00bcbf5332b7c13f9ffdf4fb
5ato <pomidkil@gmail.com>
2022-10-20 09:58:43 +0500

    work on pep8

--- unique_in_order.py
+++ unique_in_order.py
@@ -5,12 +5,12 @@ https://www.codewars.com/kata/54e6533c92449cc251001667
 from itertools import groupby
 
 
-def order1(iterable):
+def order1(iterable: str) -> list:
     iterable += 'a'
     return [iterable[i] for i in range(len(iterable)-1) if iterable[i] != iterable[i+1]]
 
 
-def order2(iterable):
+def order2(iterable: str) -> list:
     return [_ for _, g in groupby(iterable)]
 
 
